347fd99f65d2c4395e111de349d3a4b3850906b0
Panagopulos <tpanagopulos2@gmail.com>
2024-09-02 16:07:01 +0200

    fetching the data from the api with getFetchData function , resposne accepted by updateWeatherInfo, create showDisplaySection to display the proper section of the html depending on the result of the API fetch.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const cityInput = document.querySelector('.city-input');
 const searchBtn = document.querySelector('.search-btn');
 
+const weatherInfoSection = document.querySelector('.weather-info');
+const notFoundSection = document.querySelector('.not-found');
+const searchCitySection = document.querySelector('.search-city');
+
+const apiKey = '7979fa4287949928b56473d08d015c0f';
+
+// Making input & search button interactive on click, if statement to prevent empty output, blur to unfocus
 searchBtn.addEventListener('click', () => {
     if(cityInput.value.trim() !== '') {
     updateWeatherInfo(cityInput.value);
@@ -9,6 +16,7 @@ searchBtn.addEventListener('click', () => {
     }
 })
 
+// Making input interactive when user click on 'Enter' same functionality as above
 cityInput.addEventListener('keydown', (event) => {
     if(event.key === 'Enter' &&
         cityInput.value.trim()) {
@@ -18,9 +26,35 @@ cityInput.addEventListener('keydown', (event) => {
         }  
 });
 
-function getFetchData() {
-    
+
+//Using async(promise) to wait for the response/data from the openWeather API. 
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`
+
+    const response = await fetch(apiUrl);
+
+    return response.json()
 }
-function updateWeatherInfo(city) {
-    const weatherData = getFetchData()
+
+// Function which accepts the city parametr that user inputs and through api it gets the precise data of wheater or forecast
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city);
+
+    if(weatherData.cod !== 200) {
+        showDisplaySection(notFoundSection);
+        return 
+    }
+    console.log(weatherData)
+
+
+    showDisplaySection(weatherInfoSection)
+
+}
+
+function showDisplaySection(section) {
+   const sections = [weatherInfoSection, searchCitySection, notFoundSection];
+
+         sections.forEach(section => section.style.display = 'none'); // Hides all sections
+
+         section.style.display = 'flex'; // Reveals the section which is passed in the parameter
 }
